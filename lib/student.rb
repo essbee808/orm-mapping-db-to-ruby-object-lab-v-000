64d36e362ec4c836db1163d7b4a5967f2fbc9ced
Sheena Bonilla <sheena.myb@gmail.com>
2019-01-28 03:18:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -87,7 +87,7 @@ class Student
     end
   end
   
-  def self.first_x_students_in_grade_10(student_count)
+  def self.first_X_students_in_grade_10(student_count)
     binding.pry
     sql = <<-SQL 
       SELECT *
