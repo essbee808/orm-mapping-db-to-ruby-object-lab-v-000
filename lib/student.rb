d88d8554a53e668cd79c98bf0feead29e940a40f
Sheena Bonilla <sheena.myb@gmail.com>
2019-01-28 03:20:46 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -95,7 +95,7 @@ class Student
       WHERE grade = 10
     SQL
     
-    DB[:conn].execute(sql).map do |row|
+    DB[:conn].execute(sql).student_count.times.map do |row|
       self.new_from_db(row)
     end
   end
